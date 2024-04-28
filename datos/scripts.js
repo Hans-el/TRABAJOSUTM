8aa818b77b0051b5f65bfad2b51de3fb43bd5e72
Hans-el <168109764+Hans-el@users.noreply.github.com>
2024-04-28 11:39:55 -0500

    Delete datos directory

--- datos/scripts.js
+++
@@ -1,9 +0,0 @@
-// Usamos Java Script para lograr barra lateral derecha dinámica
- 
-function toggleRightSidebar() {
-    const body = document.body;
-    const rightSidebar = document.querySelector('.right-sidebar');
-
-    // Toggle la clase para ocultar/mostrar la barra lateral derecha
-    body.classList.toggle('right-sidebar-hidden');
-}
